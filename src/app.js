82a7ae602a7db7379d57b41fe327d7bb391221c8
Paul Brzeski <mail@paulbrzeski.com>
2020-01-08 17:48:59 +0800

    Autostarting background videos and lightened by default on mobile

--- src/app.js
+++ src/app.js
@@ -1,15 +1,9 @@
 $(document)
   .ready(function() {
 
-    $('#top_right, #top_left, #bottom_left, #bottom_right').hover(function(){
-      var video = $(this).find('video')[0];
-      if (video.play) video.play();
+    $('video').each(function(){
+      this.loop = true;
+      this.play();
     });
-
-    $('#top_right, #top_left, #bottom_left, #bottom_right').mouseleave(function(){
-      var video = $(this).find('video')[0];
-      if (video.pause) video.pause();
-    });
-
   })
 ;
